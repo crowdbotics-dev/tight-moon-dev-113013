6b7dfd683de90102fbeac4379934bb4366a838e9
Crowdbotics <engineering@crowdbotics.com>
2023-08-30 23:11:53 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "tight-moon-dev-113013",
+  url: "https://tight-moon-dev-113013.botics.co",
+  api: "https://tight-moon-dev-113013.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
